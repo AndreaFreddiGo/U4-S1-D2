434f770b2351c889e6fffc006f0280d47c8f15f3
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-11 16:01:26 +0100

    Aggiungere i file di progetto.

---
+++ U4-S1-D2/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿namespace U4_S1_D2.Models
+{
+    public class Persona
+    {
+        public string _nome;
+        public string _cognome;
+        public int _eta;
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            _nome = nome;
+            _cognome = cognome;
+            _eta = eta;
+        }
+    }
+
+
+}
